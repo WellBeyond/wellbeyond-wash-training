ae45ec89245fafadbdc87c391358c864258fcd64
Oluebube Princess Egbuna <egbunaoluebube@gmail.com>
2021-03-16 22:08:04 +0100

    Refactor the header component

---
+++ src/components/HeaderLogo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IonCardContent, IonCard, IonText, IonMenuButton, IonHeader } from '@ionic/react';
+import './HeaderLogo.scss'
+
+// functional component
+const HeaderLogo = (properties: any) => {
+  return (
+    <IonHeader translucent={true} className="page-header ion-no-border" text-center>
+      <IonMenuButton />
+      <IonText className="menu-text ion-hide-lg-up">{properties.menuText}</IonText>
+      <IonCard className="header-logo">
+        <IonCardContent>
+          <div className="well-beyond-logo">
+            <img src="assets/img/appicon.png" alt="WellBeyond logo" />
+            <IonText>{ properties.pageTitle }</IonText>
+          </div>
+        </IonCardContent>
+      </IonCard>
+  </IonHeader>
+  );
+}
+
+export default HeaderLogo
