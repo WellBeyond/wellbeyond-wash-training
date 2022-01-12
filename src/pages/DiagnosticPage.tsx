9416a934fe3d58fa83df90dcaae9ed138d01fbd0
Princess <princess@Princesss-MacBook-Pro.local>
2022-01-12 01:36:52 +0100

    Update form type with borehole

--- src/pages/DiagnosticPage.tsx
+++ src/pages/DiagnosticPage.tsx
@@ -15,9 +15,6 @@ import {
   IonToolbar
 } from '@ionic/react';
 
-import { Widget } from '@typeform/embed-react'
-
-
 import './DiagnosticPage.scss'
 
 import {Trans, useTranslation} from "react-i18next";
@@ -79,9 +76,7 @@ const DiagnosticPage: React.FC<DiagnosticPageProps> = ({ systems, organization, 
           <IonTitle>{t('resources.systems.name_plural')}</IonTitle>
         </IonToolbar>
       </IonHeader>
-
       <IonContent fullscreen={true}>
-          <Widget id="LV8CJAbe" className="my-form" />
         {organization && systems ?
           (<IonList>
             <IonListHeader>
